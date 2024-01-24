4fa2acea3646fb0bf976b60f81239a66cb2da688
Replit user <>
2024-01-24 23:05:17 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import shape_calculator
 from unittest import main
 
 
-rect = shape_calculator.Rectangle(5, 10)
+rect = shape_calculator.Rectangle(5, 9)
+rect2 = shape_calculator.Rectangle(3,2)
 print(rect.get_area())
 rect.set_width(3)
 print(rect.get_perimeter())
-print(rect)
-
+print(rect.get_diagonal())
+print(rect.get_picture())
+print(rect2.width)
+print(rect.get_amount_inside(rect2))
 sq = shape_calculator.Square(9)
 print(sq.get_area())
 sq.set_side(4)
+sq.set_height(5)
 print(sq.get_diagonal())
 print(sq)
 
--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,87 @@
 class Rectangle:
+  def __init__(self,width,height):
+    self.width = width
+    self.height = height
+
+  def __str__(self):
+    return f'Rectangle(width={self.width}, height={self.height})'
+
+  def set_width(self,width):
+    if isinstance(self,Square):
+      self.set_side(width)
+    else:  
+      self.width = width
+
+  def set_height(self,height):
+    if isinstance(self,Square):
+      self.set_side(height)
+    else:  
+      self.height = height
+  
+  def get_area(self):
+    return self.width * self.height
+
+  def get_perimeter(self):
+    return (2 * self.width) + (2 * self.height)
+
+  def get_diagonal(self):
+    return (self.width ** 2 + self.height ** 2) ** .5
+
+  def get_picture(self):
+    if self.width > 50 or self.height > 50:
+      return "Too big for picture."
+    picture = ''  
+    for i in range(self.height):
+      for j in range(self.width):
+        picture += '*'
+      picture += '\n'
+    return picture
+
+  def get_amount_inside(self,shape):
+    inner_width = shape.width
+    inner_height = shape.height
+    outer_width = self.width
+    outer_height = self.height
+    return self._get_amount_inside(outer_width,outer_height,inner_width,inner_height)
+
+  def get_amount_inside_with_rotation(self,shape):
+    inner_width = shape.width
+    inner_height = shape.height
+    outer_width = self.width
+    rotate_outer_width = self.height
+    rotate_outer_height = self.width
+    outer_height = self.height
+    result = self._get_amount_inside(outer_width,outer_height,inner_width,inner_height) +       self._get_amount_inside((outer_height % inner_height),outer_width,inner_width,inner_height) + self._get_amount_inside(outer_height,outer_width % inner_width,inner_width,inner_height)
+    result_rotate = self._get_amount_inside(rotate_outer_width,rotate_outer_height,inner_width,inner_height) + self._get_amount_inside((rotate_outer_height % inner_height),rotate_outer_width,inner_width,inner_height) + self._get_amount_inside(rotate_outer_height,rotate_outer_width % inner_width,inner_width,inner_height)
+    
+    return max([result,result_rotate])
+    
+  def _get_amount_inside(self,outer_width,outer_height,inner_width,inner_height,n=0):
+    width = outer_width
+    if outer_width < inner_width or outer_height < inner_height:
+      return n 
+    else:
+      while width >= inner_width:
+        n += 1
+        width -= inner_width
+      n += self._get_amount_inside(outer_width,(outer_height - inner_height),inner_width,inner_height)
+      return n 
 
 
 
 
-class Square:
+class Square(Rectangle):
+  def __init__(self,side):
+    self.side = side
+    self.width = self.side
+    self.height = self.side
+
+  def __str__(self):
+    return f'Square(side={self.side})'
+
+  def set_side(self,side):
+    self.side = side
+    self.width = side
+    self.height = side
+
+    
